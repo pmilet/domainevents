bb1dabd6da5ed1522c88478d1f1c3f55c47d86bf
Pierre Miguel Millet <59197536@ibercaja.es>
2017-10-14 12:01:18 +0200

    copyright comments added

--- tst/PiedraPapelTijeraApp/Match.cs
+++ tst/PiedraPapelTijeraApp/Match.cs
@@ -1,4 +1,6 @@
-﻿using pmilet.DomainEvents;
+﻿// Copyright (c) Pierre Milet. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+using pmilet.DomainEvents;
 using System;
 using System.Collections.Generic;
 using System.Linq;
